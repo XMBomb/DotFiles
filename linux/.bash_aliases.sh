fcd7c8a719c6f00d275c6cd88305c9a308707a4c
Milan Bharanya <spam@trash.li>
2016-10-18 20:44:38 +0200

    initial commit

---
+++ linux/.bash_aliases.sh
@@ -0,0 +1,31 @@
+# ------------- History -------------------------
+
+export HISTSIZE=100000                   # big big history
+export HISTFILESIZE=100000               # big big history
+
+# Avoid duplicates
+export HISTCONTROL=ignoredups:erasedups  
+# When the shell exits, append to the history file instead of overwriting it
+shopt -s histappend
+
+# After each command, append to the history file and reread it
+export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+
+# ---------------------------------------------
+
+alias chaos="ssh chaos"
+alias gaia="ssh gaia"
+alias zelos="ssh zelos"
+alias tethys="ssh tethys"
+alias iris="ssh iris"
+alias pandora="ssh pandora"
+alias ladon="ssh ladon"
+alias heracles="ssh heracles"
+alias zeus="ssh root@zeus"
+alias artemis="ssh artemis"
+alias jrnl="ssh xmbomb@192.168.1.8 jrnl"
+alias lla="ls -lah"
+alias st="sudo ntpdate pool.ntp.org"
+alias www="cd /var/www/"
+alias codefortune="fgrep '/* ' /usr/src/linux* -r | cut -d '*' -f 2 | head -$((RANDOM)) | tail -1"
+alias ldir="ls -l | grep "^d" | awk '{print $9}'"
